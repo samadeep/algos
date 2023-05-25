2564eda9046d3da6a6c28564785781aee42d2226
samadeep <64741761+samadeep@users.noreply.github.com>
2023-05-25 19:39:30 +0530

    Update fenwick_template.cpp

--- fenwick_tree/fenwick_template.cpp
+++ fenwick_tree/fenwick_template.cpp
@@ -1,3 +1,16 @@
+// The class has two member variables:
+
+// fenw is a vector of type T that represents the Fenwick tree. It will store the cumulative values used to efficiently calculate prefix sums.
+// n is an integer that represents the size of the Fenwick tree.
+
+// The constructor Fenwick(int _n) takes an integer _n and initializes the n variable with the provided value. It also resizes the fenw vector to have a size equal to n.
+
+// The modify member function is responsible for updating the Fenwick tree. It takes an index x and a value v. The function adds the value v to the element at index x 
+// in the Fenwick tree and propagates the changes to the necessary indices in the tree using bitwise operations and a while loop.
+
+// The get member function is used to calculate the prefix sum up to a given index x. It takes an index x as an argument and returns the cumulative sum of 
+// the elements from index 0 to x in the Fenwick tree. It iteratively accumulates the values by traversing the tree in reverse order using bitwise operations and a while loop.
+
 #include <bits/stdc++.h>
 using namespace std;
 
