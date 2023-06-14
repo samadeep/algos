38576c25b50a99196ad3f3a2b7cef04f98c679e5
samadeep <64741761+samadeep@users.noreply.github.com>
2023-06-14 15:03:36 +0530

    Create Traversal_template.cpp
    Template for Tree Traversal

---
+++ Trees/Traversal_template.cpp
@@ -0,0 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */ 
+
+// Source : https://leetcode.com/problems/binary-tree-postorder-traversal/solutions/504598/an-iterative-3-in-1-template-for-preinpost-order-traversal/
+
+        stack<TreeNode*> stack{};
+        TreeNode* node = root;
+        TreeNode* prev = nullptr;
+
+        while (node or not stack.empty()) {
+            while (node) {
+                // Pre-order region {
+                //   do_something();
+                // }
+                stack.push(node);
+                node = node->left;
+            }
+            node = stack.top();
+
+            if (not node->right or prev != node->right) {
+                // In-order region {
+                //   do_something();
+                // }
+                if (node->right) {
+                    node = node->right;
+                    continue;
+                }
+            }
+
+            if (not node->right or prev == node->right) {
+                // Post-order region {
+                //   do_something();
+                // }
+                stack.pop();
+                prev = node;
+                node = nullptr;
+            }
+        }
